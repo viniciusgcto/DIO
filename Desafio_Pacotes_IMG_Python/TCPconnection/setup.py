2411007c77a0543c8fa666a9bb2241363ab48764
viniciusgcto <viniciusgcto@gmail.com>
2022-03-18 13:39:51 -0300

    version update

--- Desafio_Pacotes_IMG_Python/TCPconnection/setup.py
+++ Desafio_Pacotes_IMG_Python/TCPconnection/setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="TCPconnection",
-    version="0.0.1",
+    version="0.0.2",
     author="Vinicius Costa",
     author_email="viniciusgcto@gmail.com",
     description="Simple TCP connection",
